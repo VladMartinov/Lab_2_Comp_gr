1b117e7a5e6021ace46225bce6aa04a5a9f5a56c
VladMartinov <vladmartinov1704@mail.ru>
2022-10-12 10:28:33 +0300

    Добавьте файлы проекта.

---
+++ Lab_2(Comp. gr.)/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Lab_2_Comp.gr._
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(5, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.81374F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.18625F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(906, 485);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.button4, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 409);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(900, 73);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button4.Location = new System.Drawing.Point(725, 20);
+            this.button4.Margin = new System.Windows.Forms.Padding(50, 20, 50, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 33);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(500, 20);
+            this.button3.Margin = new System.Windows.Forms.Padding(50, 20, 50, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Inches";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.buttonInch_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(275, 20);
+            this.button2.Margin = new System.Windows.Forms.Padding(50, 20, 50, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Millimetrs";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.buttonMillimeter_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(50, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(50, 20, 50, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pixels";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttonPixel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 400);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 484);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(931, 531);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Lab_2(Comp. gr.)/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab_2_Comp.gr._
+{
+    public partial class Form1 : Form
+    {
+        // Цвет пользователя (можно будет добавить текст боксы где будут значения для r, g, b и записывать в данную переменную)
+        Color usersColor = Color.FromArgb(0, 255, 0);
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Задаем цвет заднему фону pictureBox1
+            pictureBox1.BackColor = Color.FromKnownColor(KnownColor.PowderBlue);
+        }
+
+        // метод для деления pictureBox1 визуально на 4 части
+        private void print_Bg()
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            // Очистка pictureBox1
+            g.Clear(pictureBox1.BackColor);
+
+            // Задается размер для 1/4 размера PictureBox
+            SizeF smallSquareSize = new
+                                    SizeF(this.pictureBox1.Width / 2,
+                                    this.pictureBox1.Height / 2);
+            //Создаём карандаш
+            Pen myPen = new Pen(Color.FromName("Orange"), 2);
+            // Цикл из 2-х итераций по оси Y
+            for (int y = 0; y < 2; y++)
+            {
+                // 2-а шага по горизонтали по оси X
+                for (int x = 0; x < 2; x++)
+                {
+                    // размеры квадрата
+                    Rectangle rc1 = new Rectangle(x * (int)smallSquareSize.Width,
+                                                    y * (int)smallSquareSize.Height,
+                                                    (int)smallSquareSize.Width,
+                                                    (int)smallSquareSize.Height);
+                    // отображение квадрата
+                    g.DrawRectangle(myPen, rc1);
+                }
+            }
+        }
+
+        private void buttonPixel_Click(object sender, EventArgs e)
+        {
+            // Вызов метода print_Bg
+            print_Bg();
+
+            Graphics g = pictureBox1.CreateGraphics();
+
+            // в режиме Pixel
+            g.PageUnit = GraphicsUnit.Pixel;
+
+            // Степень
+            float power = 5;
+
+            // Кол-во точек на граффике
+            var count = 100;
+
+            // Шаг между точками
+            var step = 0.05;
+
+            // Запись в пременную point массива точек
+            var points = Enumerable.Range(0, count) // Метод вернет массив чисел от 0 до 99
+                .Select(x => step * x - step * count / 2) // пересчитаю x, получится промежуток [-5;5] с шагом 0.05
+                .Select(x => new PointF((float)x, (float)Math.Pow(x, power)));  // считаем y, и формируем массив точек в декартовой системе координат
+
+            Color myPen = usersColor; // Выбор цвета для карандаша
+
+            var blackPen = new Pen(myPen, 1); // создание карандаша
+
+            // Вывод на середину экрана в Pixel 
+            g.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+            // Переворот графика
+            g.ScaleTransform(1, -1);
+
+            // приближаем изобиажение
+            g.ScaleTransform(g.DpiX / 2.54f, g.DpiY / 2.54f);
+
+            // Т.к. при приближении увеличилась и ширина карандаша то клонируем её для изменения её параметров
+            var penTransform = g.Transform.Clone();
+
+            // обращаем матрицу penTransform
+            penTransform.Invert();
+
+            // фиксируем матрицу перехода у пера, 
+            // как обратную к матрице перехода e.Graphics 
+            blackPen.Transform = penTransform;
+
+            // Отображение графика
+            g.DrawLines(blackPen, points.ToArray());
+        }
+
+        private void buttonMillimeter_Click(object sender, EventArgs e)
+        {
+            // ресуем сетку
+            print_Bg();
+
+            Graphics g = pictureBox1.CreateGraphics();
+
+            // в режиме Millimeter
+            g.PageUnit = GraphicsUnit.Millimeter;
+
+            // Степень
+            float power = 5;
+
+            // Кол-во точек на граффике
+            var count = 100;
+
+            // Шаг между точками
+            var step = 0.05f;
+
+            // Запись в пременную point массива точек
+            var points = Enumerable.Range(0, count) // Метод вернет массив чисел от 0 до 99
+                .Select(x => step * x - step * count / 2) // пересчитаю x, получится промежуток [-2;2] с шагом 0.05f
+                .Select(x => new PointF(x, (float)Math.Pow(x, power)));  // считаем y, и формируем массив точек в декартовой системе координат
+
+            Color myPen = usersColor; // Выбор цвета для карандаша
+
+            var blackPen = new Pen(myPen, 0.5f); // создание карандаша
+
+            // Вывод на середину экрана в Millimeter
+            g.TranslateTransform((0.5f * pictureBox1.Width) * (float) 0.265, (0.5f * pictureBox1.Height) * (float)0.265);
+
+            // Переворот графика
+            g.ScaleTransform(1, -1);
+
+            // приближаем изобиажение
+            g.ScaleTransform(g.DpiX / 2.54f, g.DpiY / 2.54f);
+
+            // Т.к. при приближении увеличилась и ширина карандаша то клонируем её для изменения её параметров
+            var penTransform = g.Transform.Clone();
+
+            // обращаем матрицу penTransform
+            penTransform.Invert();
+
+            // фиксируем матрицу перехода у пера, 
+            // как обратную к матрице перехода e.Graphics 
+            blackPen.Transform = penTransform;
+
+            // Отображение графика
+            g.DrawLines(blackPen, points.ToArray());
+        }
+
+        private void buttonInch_Click(object sender, EventArgs e)
+        {
+            // ресуем сетку
+            print_Bg();
+
+            Graphics g = pictureBox1.CreateGraphics();
+
+            // в режиме Inch
+            g.PageUnit = GraphicsUnit.Inch;
+
+            // Степень
+            float power = 5;
+
+            // Кол-во точек на граффике
+            var count = 100;
+            
+            // Шаг между точками
+            var step = 0.05f;
+
+            // Запись в пременную point массива точек
+            var points = Enumerable.Range(0, count) // Метод вернет массив чисел от 0 до 99
+                .Select(x => step * x - step * count / 2) // пересчитаю x, получится промежуток [-5;5] с шагом 0.05f
+                .Select(x => new PointF(x, (float)Math.Pow(x, power)));  // считаем y, и формируем массив точек в декартовой системе координат
+
+            Color myPen = usersColor;   // Выбор цвета для карандаша
+
+            var blackPen = new Pen(myPen, 0.01f);   // создание карандаша
+
+            // Вывод на середину экрана в дюймах
+            g.TranslateTransform((0.5f * pictureBox1.Width) / 96, (0.5f * pictureBox1.Height) / 96);
+
+            // Переворот графика
+            g.ScaleTransform(1, -1);
+            
+            // Отображение графика
+            g.DrawLines(blackPen, points.ToArray());
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            // Удаление всех рисунков
+            g.Clear(pictureBox1.BackColor);
+        }
+
+
+    }
+}
+    
